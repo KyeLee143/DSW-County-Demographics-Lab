0c9ad48da9b24d0cbb88dd9083ead55da0a8d330
KyeLee143 <keilipalmares8455@gmail.com>
2019-10-29 10:01:22 -0700

    finished 3

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -4,10 +4,10 @@ def main():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
     print(high_income_counties(counties))
-    #print(alphabetically_first_county(counties))
+    print(alphabetically_first_county(counties))
     #print(county_most_under_18(counties))
     #print(percent_most_under_18(counties))
-    #print(lowest_median_income(counties))
+    print(lowest_median_income(counties))
     #print(state_with_most_counties(counties))
 
 def high_income_counties(counties):
@@ -20,12 +20,21 @@ def high_income_counties(counties):
 
 def lowest_median_income(counties):
     """Return a name of a county with the lowest median household income"""
+    lowest_median_income = counties[5]['Income']['Median Household Income'] 
+    for data in counties:
+        if data ['Income']['Median Household Income'] < lowest_median_income:
+            lowest_median_income = data ['Income']['Median Household Income']
+    return lowest_median_income
     
 
 def alphabetically_first_county(counties):
     """Return the county with the name that comes first alphabetically."""
     #Hint: you can use < to compare strings in Python. ex) "cat" < "dog" gives the value True
-
+    alphabetically = counties[0]['County']
+    for data in counties:
+        if data ['County'] < alphabetically:
+            alphabetically = data ['County']
+    return alphabetically
     
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""    
